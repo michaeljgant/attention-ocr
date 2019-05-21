5ec36409dd8be446868e04ba62e7c2c93800bd16
Gant, Michael <Michael.Gant@F-Secure.com>
2019-05-21 15:44:57 +0200

    more functions run and output commented

--- debugging.py
+++ debugging.py
@@ -49,3 +49,55 @@ model_train.max_width
 model_train.height
 
 model_train.encoder_size
+model_train.decoder_size
+
+model_train.conv_output
+
+model_train.attention_decoder_model.output
+
+
+model_train.encoder_masks
+model_train.img_data
+
+
+len(model_train.decoder_inputs)
+len(model_train.target_weights)
+
+len(model_train.attention_decoder_model.target_weights)
+len(model_train.attention_decoder_model.decoder_inputs)
+len(model_train.attention_decoder_model.encoder_masks)
+len(model_train.attention_decoder_model.output)
+model_train.attention_decoder_model.output[0].shape
+
+
+s_gen = DataGen(
+            data_path, model_train.buckets,
+            , max_width=self.max_original_width
+        )
+
+
+
+
+"""
+Out[40]: 1300
+model_train.max_width
+Out[41]: 555
+model_train.height
+Out[42]: <tf.Tensor 'Const:0' shape=() dtype=int32>
+model_train.encoder_size
+Out[43]: 139
+model_train.decoder_size
+Out[44]: 21
+model_train.conv_output
+Out[46]: <tf.Tensor 'Squeeze:0' shape=(?, 139, 512) dtype=float32>
+model_train.img_data
+Out[50]: <tf.Tensor 'map/TensorArrayStack/TensorArrayGatherV3:0' shape=(?, ?, 555, 1) dtype=float32>
+model_train.attention_decoder_model.output[0].shape
+Out[61]: TensorShape([Dimension(None), Dimension(39)])
+Out[57]: 22
+len(model_train.attention_decoder_model.encoder_masks)
+Out[58]: 140
+len(model_train.attention_decoder_model.output)
+Out[59]: 21
+
+"""
